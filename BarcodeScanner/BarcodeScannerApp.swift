78212fb0f4f6d699e617f2f6b2708398a78a6546
yilmazemre61 <emreyilmz1@gmail.com>
2024-02-27 17:42:17 -0500

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Emre Yilmaz on 2024-02-27.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarcodeScanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BarcodeScanner
+//
+//  Created by Emre Yilmaz on 2024-02-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
